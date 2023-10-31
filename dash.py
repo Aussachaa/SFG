b48bc9cc847c8c9f85b5fbea78a76e1d812ae974
Sax <127007056+Aussachaa@users.noreply.github.com>
2023-10-31 09:54:43 +0700

    add

--- dash.py
+++ dash.py
@@ -18,52 +18,50 @@ def format_number(number):
 
 
 st.title(" :bar_chart: Performance SFG 3000")
-st.markdown(
-    '<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
+st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
 
-path_file = r'https://github.com/Aussachaa/SFG/raw/main/Peformance_Data.xlsx'
-df_chan = pd.read_excel(path_file, engine="openpyxl", sheet_name='Channel')
-df_chan = df_chan[df_chan['ACC_Name'].isin(['Net Sale', 'Gross Profit'])]
+path_db = r"https://github.com/Aussachaa/SFG/raw/main/Database.xlsx"
+df = pd.read_excel(path_db, engine="openpyxl", sheet_name='Per19-23')
+df['Year'] = df['Year'].astype('str')
+
+# df_chan = df_chan[df_chan['ACC_Name'].isin(['Net Sale', 'Gross Profit'])]
 
 # df_chan_format = df_chan.copy()
 # df_chan_format.loc[:, 'JAN': 'Q4'] = df_chan_format.loc[:,'JAN': 'Q4'].applymap(format_number)
 
-df_chan['AT_FC'] = df_chan['AT_FC'].astype('str')
+# df_chan['AT_FC'] = df_chan['AT_FC'].astype('str')
 
 st.sidebar.header("Choose your filter: ")
 
 # Year
-year = st.sidebar.selectbox('Select Year:', df_chan['AT_FC'].unique())
-df_chan2 = df_chan[df_chan['AT_FC'].isin([year])]
+year = st.sidebar.selectbox('Select Year:', df['Year'].unique())
+df = df[df['Year'].isin([year])]
 
 # Brand
-brand = st.sidebar.multiselect('Pick your Brand:', df_chan['Brand'].unique())
+brand = st.sidebar.multiselect('Pick your Brand:', df['Brand'].unique())
 if not brand:
-    df_chan3 = df_chan2.copy()
+    df2 = df.copy()
 else:
-    df_chan3 = df_chan2[df_chan2['Brand'].isin(brand)]
+    df2 = df[df['Brand'].isin(brand)]
 
-# Channel
-channel = st.sidebar.multiselect(
-    'Pick your Channel:', df_chan2['Channel'].unique())
-if not channel:
-    df_chan4 = df_chan3.copy()
-else:
-    df_chan4 = df_chan3[df_chan3['Channel'].isin(channel)]
+# ACC Name
+acc = st.sidebar.selectbox('Select ACC:', df2['ACC Name'].unique())
+df2 = df2[df2['ACC Name'].isin([acc])]
 
+# Barh graph
+df_tot_ns = df2.sort_values(by=['M01-12'], ascending=False)
 
-with st.expander("Data M01-12_ViewData"):
-    # st.markdown('### Data M01-12')
-    df_chan2_ytd = df_chan4.loc[:, [
-        'AT_FC', 'Group_Brand', 'Brand', 'Channel', 'ACC_Name', 'M01-12']]
-    df_chan2_ytd_unstk = df_chan2_ytd.set_index(
-        ['ACC_Name', 'AT_FC', 'Group_Brand', 'Brand', 'Channel'])
-    df_chan2_ytd_unstk = df_chan2_ytd_unstk.unstack(level=0)
-    df_chan2_ytd_unstk = df_chan2_ytd_unstk.droplevel([0], axis=1)
-    df_chan2_ytd_unstk.columns.name = None
-    df_chan2_ytd_unstk = df_chan2_ytd_unstk[['Net Sale', 'Gross Profit']]
-    df_chan2_ytd_unstk.reset_index(inplace=True)
-    #df_chan2_ytd_unstk.set_index('AT_FC', inplace=True)
-    cm = sns.light_palette("green", as_cmap=True)
-    st.dataframe(df_chan2_ytd_unstk.style.background_gradient(
-        cmap=cm, axis=0), use_container_width=True)
+st.write(df_tot_ns)
+
+fig = px.bar(df_tot_ns, x='Brand', y='M01-12')
+for index, row in df_tot_ns.iterrows():
+    fig.add_annotation(
+        x=row['Brand'],
+        y=row['M01-12'],
+        showarrow=False,
+        text=str(round(row['M01-12'] / 1000, 1)) + 'M',
+        bgcolor="rgba(255, 255, 0, 0.5)")
+
+fig.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False)
+
+st.plotly_chart(fig, use_container_width=True)
